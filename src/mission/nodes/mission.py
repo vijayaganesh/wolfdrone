35ac8c47b058eee4417a152ed3e4f61b8d61e95f
vijayaganesh <mvijayaganesh94@gmail.com>
2018-03-29 14:56:53 -0400

    Abstract Base Class for all missions

--- src/mission/nodes/mission.py
+++ src/mission/nodes/mission.py
@@ -6,4 +6,18 @@ Created on Sat Mar 24 23:38:23 2018
 @author: vijayaganesh
 """
 
-
+from abc import ABC, abstractmethod
+class Mission(ABC):
+  
+  @abstractmethod
+  def startup(self):
+    pass
+  
+  @abstractmethod
+  def run(self):
+    pass
+  
+  @abstractmethod
+  def cleanup(self):
+    pass
+  
