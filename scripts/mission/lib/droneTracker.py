988d75f3ba9dcd99f568b4cffd7e402f75101052
vijayaganesh <mvijayaganesh94@gmail.com>
2018-04-13 15:03:11 -0400

    Fixed errors caused due to migrating to pycharm

--- scripts/mission/lib/droneTracker.py
+++ scripts/mission/lib/droneTracker.py
@@ -9,9 +9,11 @@ Created on Thu Mar 29 13:26:06 2018
 
 
 class DroneTracker(object):
+
   """
     TODO: Kalman Filter based image tracker to adjust the drone velocity on descend to land on top of the drone to be picked up.
 
 
-  """  
-  
+  """
+
+  def __init__
