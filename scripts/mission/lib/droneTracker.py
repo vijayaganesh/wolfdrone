9a0e0985fc51d9c43e009fede9640bf86e3df572
vijayaganesh <mvijayaganesh94@gmail.com>
2018-04-13 15:06:20 -0400

    Drone Tracker code

--- scripts/mission/lib/droneTracker.py
+++ scripts/mission/lib/droneTracker.py
@@ -5,10 +5,11 @@ Created on Thu Mar 29 13:26:06 2018
 
 @author: vijayaganesh
 """
+from position import Positions
+from camera import SimCam , RealCam
+import cv2
 
-
-
-class DroneTracker(object):
+class DroneTracker(Positions):
 
   """
     TODO: Kalman Filter based image tracker to adjust the drone velocity on descend to land on top of the drone to be picked up.
@@ -16,4 +17,16 @@ class DroneTracker(object):
 
   """
 
-  def __init__
+  def __init__(self,use_sim = True):
+    if use_sim:
+      self.cam = SimCam()
+    else:
+      self.cam = RealCam()
+  
+  def run(self):
+    img = self.cam.get_frame()  
+    if img is not None:
+      cv2.imshow('Image Feed',self.cam.get_frame())
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        exit()
